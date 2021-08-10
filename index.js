caa7f86ff8c0ee90f08c7e75e57d5878c17b8050
Nick Grealy <nickgrealy@gmail.com>
2021-08-10 14:16:36 +1000

    Exampel.

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ContainerApi } = require('./src/api/containerApi')
+const DockerEngineApi = require('./src/index')
+
+// const ApiClient = require('./dist/ApiClient')
+
+// ApiClient.instance = new ApiClient({ basePath: 'http+unix://%2Fvar%2Frun%2Fdocker.sock' })
+
+const init = async () => {
+    /** @type {module:src/api/ContainerApi */
+    var api = new DockerEngineApi.ContainerApi()
+    // var opts = {
+    //     'body': new DockerEngineApi.UNKNOWN_BASE_TYPE() // {UNKNOWN_BASE_TYPE} 
+    // };
+
+    const result = await api.containerList()
+    console.log('containers', result)
+}
+
+init()
