b651ca1593095169982b0e476efcc94279527793
Nick Grealy <nickgrealy@gmail.com>
2021-08-10 11:02:54 +1000

    cleaned worksapce.

--- scripts/generate_swaggercodegen.sh
+++ scripts/generate_swaggercodegen.sh
@@ -6,7 +6,7 @@ echo Generating NodeJS client for spec $YAML_URL...
 
 docker run --rm -v "${PWD}:/local" swaggerapi/swagger-codegen-cli generate \
     -i $YAML_URL \
-    -l typescript-node \
-    -o /local/src
+    -l javascript \
+    -o /local
 
-    # -l javascript \
+    # -l typescript-node \
